990f90b56adf2cf904c376213b0fda853f263e93
David-127 <71015265+David-127@users.noreply.github.com>
2021-05-10 15:19:51 +0200

    Add project files.

---
+++ HighLow/Card.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighLow
+{
+    class Card
+    {
+        //fält
+        value cardValue;
+        suit cardSuit;
+
+        //konstruktor
+        public Card(value cardValue, suit cardSuit)
+        {
+            this.cardValue = cardValue;
+            this.cardSuit = cardSuit;
+        }
+        //metoder
+        public void CardInfo()
+        {
+
+        }
+        public void setCardValue(value cardValue)
+        {
+            this.cardValue = cardValue;
+        }
+
+        public value getCardValue()
+        {
+            return cardValue;
+        }
+
+        public void setCardSuit(suit cardSuit)
+        {
+            this.cardSuit = cardSuit;
+        }
+
+        public suit getCardSuit()
+        {
+            return cardSuit;
+        }
+
+    }
+}
---
+++ HighLow/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HighLow
+{
+    public enum color { Black, Red }
+    public enum value
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public enum suit
+    {
+        Hearts,
+        Spades,
+        Diamond,
+        Club
+    }
+    class Program
+    {
+        static List<Card> deck = new List<Card>();
+        static void Main(string[] args)
+        {
+            
+
+
+            bool playing = true;
+            while (playing == true)
+            {
+                int points = 0;
+                Console.Clear();
+                drawDeck();
+                drawCard();
+                cardChooser();
+                Console.ReadLine();
+            }
+
+
+        }
+
+        static void drawDeck()
+        {
+            deck.Clear();
+
+            for (int i = 0; i < 13; i++)
+            {
+                deck.Add(new Card((value)i, suit.Club));
+                deck.Add(new Card((value)i, suit.Diamond));
+                deck.Add(new Card((value)i, suit.Hearts));
+                deck.Add(new Card((value)i, suit.Spades));
+
+            }
+        }
+        static value drawCard()
+        {
+            Random rnd = new Random();
+            var cardNumber = rnd.Next(0, deck.Count);
+            var cardVal = deck[cardNumber].getCardValue();
+            var cardSuit = deck[cardNumber].getCardSuit();
+
+            Console.WriteLine(cardVal + " of " + cardSuit);
+
+            deck.RemoveAt(cardNumber);
+
+            return cardVal;
+
+        }
+
+        static void cardChooser()
+        {
+            Console.WriteLine("Press 1 to select LOW");
+            Console.WriteLine("Press 2 to HIGH");
+            Console.WriteLine("Press 3 to END GAME");
+                var chooseCard = 0;
+
+            switch (chooseCard)
+            {
+
+                case 0:
+
+                    break;
+
+                case 1:
+                    break;
+
+                case 2:
+                        Environment.Exit(0);
+                    break;
+            }
+            }
+
+        }
+
+    }
+
